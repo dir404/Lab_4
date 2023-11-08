83480ce3b133590261d7151b737ce5815b954470
SUDO SU <130244638+dir404@users.noreply.github.com>
2023-11-08 15:37:57 +0300

    Add files via upload

---
+++ Computer/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Computer
+{
+    public string IPAddress { get; set; }
+    public int Power { get; set; }
+    public string OperatingSystem { get; set; }
+
+    public Computer(string ipAddress, int power, string os)
+    {
+        IPAddress = ipAddress;
+        Power = power;
+        OperatingSystem = os;
+    }
+}
+
+// Клас Сервер
+public class Server : Computer, IConnectable
+{
+    public Server(string ipAddress, int power, string os) : base(ipAddress, power, os)
+    {
+    }
+
+    public void Connect(Computer target)
+    {
+        // Логіка підключення сервера до іншого комп'ютера
+        Console.WriteLine($"Сервер {IPAddress} підключається до {target.IPAddress}.");
+    }
+
+    public void Disconnect(Computer target)
+    {
+        // Логіка відключення сервера від іншого комп'ютера
+        Console.WriteLine($"Сервер {IPAddress} відключається від {target.IPAddress}.");
+    }
+
+    public void TransmitData(Computer target, string data)
+    {
+        // Логіка передачі даних від сервера до іншого комп'ютера
+        Console.WriteLine($"Сервер {IPAddress} передає дані до {target.IPAddress}: {data}");
+    }
+}
+
+public class Workstation : Computer, IConnectable
+{
+    public Workstation(string ipAddress, int power, string os) : base(ipAddress, power, os)
+    {
+    }
+
+    public void Connect(Computer target)
+    {
+        // Логіка підключення робочої станції до іншого комп'ютера
+        Console.WriteLine($"Робоча станція {IPAddress} підключається до {target.IPAddress}.");
+    }
+
+    public void Disconnect(Computer target)
+    {
+        // Логіка відключення робочої станції від іншого комп'ютера
+        Console.WriteLine($"Робоча станція {IPAddress} відключається від {target.IPAddress}.");
+    }
+
+    public void TransmitData(Computer target, string data)
+    {
+        // Логіка передачі даних від робочої станції до іншого комп'ютера
+        Console.WriteLine($"Робоча станція {IPAddress} передає дані до {target.IPAddress}: {data}");
+    }
+}
+
+public class Router : Computer, IConnectable
+{
+    public Router(string ipAddress, int power, string os) : base(ipAddress, power, os)
+    {
+    }
+
+    public void Connect(Computer target)
+    {
+        // Логіка підключення маршрутизатора до іншого комп'ютера
+        Console.WriteLine($"Маршрутизатор {IPAddress} підключається до {target.IPAddress}.");
+    }
+
+    public void Disconnect(Computer target)
+    {
+        // Логіка відключення маршрутизатора від іншого комп'ютера
+        Console.WriteLine($"Маршрутизатор {IPAddress} відключається від {target.IPAddress}.");
+    }
+
+    public void TransmitData(Computer target, string data)
+    {
+        // Логіка передачі даних від маршрутизатора до іншого комп'ютера
+        Console.WriteLine($"Маршрутизатор {IPAddress} передає дані до {target.IPAddress}: {data}");
+    }
+}
+
+public interface IConnectable
+{
+    void Connect(Computer target);
+    void Disconnect(Computer target);
+    void TransmitData(Computer target, string data);
+}
+
+public class Network
+{
+    public void SimulateInteraction(IConnectable device1, IConnectable device2, string data)
+    {
+        // Моделюємо взаємодію між пристроями
+        device1.Connect((Computer)device2);
+        device1.TransmitData((Computer)device2, data);
+        device1.Disconnect((Computer)device2);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var server = new Server("192.168.1.1", 1000, "Windows Server");
+        var workstation = new Workstation("192.168.1.2", 500, "Windows 10");
+        var router = new Router("192.168.1.3", 200, "RouterOS");
+
+        var network = new Network();
+        network.SimulateInteraction(server, workstation, "Hello, Workstation!");
+        network.SimulateInteraction(workstation, router, "Hi, Router!");
+        
+        Console.ReadLine();
+    }
+}
---
+++ LivingOrganism/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LivingOrganism
+{
+    public double Energy { get; set; }
+    public int Age { get; set; }
+    public double Size { get; set; }
+
+    public LivingOrganism(double energy, int age, double size)
+    {
+        Energy = energy;
+        Age = age;
+        Size = size;
+    }
+}
+
+public class Animal : LivingOrganism, IPredator, IReproducible
+{
+    public Animal(double energy, int age, double size) : base(energy, age, size)
+    {
+    }
+
+    public void Hunt(LivingOrganism target)
+    {
+        // Логіка полювання
+        Console.WriteLine("Тварина полює на інших організмів.");
+    }
+
+    public void Reproduce()
+    {
+        // Логіка розмноження тварин
+        Console.WriteLine("Тварина розмножується.");
+    }
+}
+
+// Клас Рослина
+public class Plant : LivingOrganism, IReproducible
+{
+    public Plant(double energy, int age, double size) : base(energy, age, size)
+    {
+    }
+
+    public void Reproduce()
+    {
+        // Логіка розмноження рослин
+        Console.WriteLine("Рослина розмножується.");
+    }
+}
+
+// Клас Мікроорганізм
+public class Microorganism : LivingOrganism, IReproducible
+{
+    public Microorganism(double energy, int age, double size) : base(energy, age, size)
+    {
+    }
+
+    public void Reproduce()
+    {
+        // Логіка розмноження мікроорганізмів
+        Console.WriteLine("Мікроорганізм розмножується.");
+    }
+}
+
+// Інтерфейс для розмноження
+public interface IReproducible
+{
+    void Reproduce();
+}
+
+// Інтерфейс для полювання
+public interface IPredator
+{
+    void Hunt(LivingOrganism target);
+}
+
+// Клас Екосистема
+public class Ecosystem
+{
+    private List<LivingOrganism> organisms = new List<LivingOrganism>();
+
+    public void AddOrganism(LivingOrganism organism)
+    {
+        organisms.Add(organism);
+    }
+
+    public void SimulateInteraction()
+    {
+        // Логіка взаємодії між організмами в екосистемі
+        foreach (var organism in organisms)
+        {
+            if (organism is IPredator)
+            {
+                var predator = (IPredator)organism;
+                foreach (var target in organisms)
+                {
+                    if (target != organism)
+                    {
+                        predator.Hunt(target);
+                    }
+                }
+            }
+
+            if (organism is IReproducible)
+            {
+                var reproducible = (IReproducible)organism;
+                reproducible.Reproduce();
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var lion = new Animal(100, 5, 1.5);
+        var deer = new Animal(80, 3, 1.0);
+        var tree = new Plant(50, 10, 5.0);
+        var bacteria = new Microorganism(30, 1, 0.01);
+
+        var ecosystem = new Ecosystem();
+        ecosystem.AddOrganism(lion);
+        ecosystem.AddOrganism(deer);
+        ecosystem.AddOrganism(tree);
+        ecosystem.AddOrganism(bacteria);
+
+        ecosystem.SimulateInteraction();
+        Console.ReadLine();
+    }
+}
